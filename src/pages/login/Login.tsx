9423d7904cc41947681753f2814258b2c31085b8
bspang <bspangler21@alumni.uwosh.edu>
2023-04-08 01:36:01 -0500

    Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,41 @@
-import { DefaultButton, Stack, TextField } from "@fluentui/react";
+import { DefaultButton, Label, Stack, TextField } from "@fluentui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 // import * as golfLogo from "../../assets/golf.png";
 
 const loginStackTokens = {
 	childrenGap: 30,
 };
 
+const forgotLinkStyles = {
+	color: "blue",
+};
+
 export default function Login() {
+	const [username, setUsername] = useState("");
+
 	return (
 		<>
 			<Stack tokens={loginStackTokens}>
-				<TextField placeholder="Username"></TextField>
-				<TextField placeholder="Password"></TextField>
-				<DefaultButton primary={true}>Login</DefaultButton>
+				<TextField
+					placeholder="Username"
+					required
+					onChange={(e, newValue) => {
+						setUsername(newValue || "");
+					}}
+					value={username}
+				></TextField>
+				<TextField placeholder="Password" required></TextField>
+				<DefaultButton
+					primary={true}
+					onClick={() => console.log("submitted username ", username)}
+				>
+					Login
+				</DefaultButton>
+				<Link to={`/`} className="forgot-link">
+					<Label>Forgot your username or password?</Label>
+				</Link>
 			</Stack>
 		</>
 	);
