9cf8c9803d7b0fe57ab509212d5b94fa64026ee5
bspang <bspangler21@alumni.uwosh.edu>
2023-04-09 00:26:15 -0500

    Delete lowdb setup

--- src/routes/client.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/home", (req, res) => {
-	res.sendFile("Home.tsx", { root: "pages" });
-	console.log("router running...");
-});
-
-module.exports = router;
