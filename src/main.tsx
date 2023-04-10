df173383849ac8ab341378723038f90e84c3c17d
BSPANGLER <bspangler21@alumni.uwosh.edu>
2023-04-09 22:59:50 -0500

    try to install and use mongodb

--- src/main.tsx
+++ src/main.tsx
@@ -5,28 +5,33 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/login/Login";
+import Schedule from "./pages/schedule/Schedule";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
+  {
+    path: "/",
 
-		element: <App />,
-	},
+    element: <App />,
+  },
 
-	{
-		path: "/home",
-		element: <Home />,
-	},
-	{
-		path: "/login",
-		element: <Login />,
-	},
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/schedule",
+    element: <Schedule />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		{/* Will cause duplication if not commented out */}
-		{/* <App /> */}
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    {/* Will cause duplication if not commented out */}
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,5 +1,46 @@
-function Schedule() {
-	return <div></div>;
+import { Label } from "@fluentui/react";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri =
+  "mongodb+srv://bspangler21:CM2xP2C2Ul5jLf7l@spangdev.xsqup9m.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// client.connect();
+
+async function run() {
+  //   try {
+  //     await client.connect();
+  //     // Send a ping to confirm a successful connection
+  //     await client.db("admin").command({ ping: 1 });
+  //     console.log(
+  //       "Pinged your deployment. You successfully connected to MongoDB!"
+  //     );
+  //   } finally {
+  //     // Ensures that the client will close when you finish/error
+  //     await client.close();
+  //   }
+  // }
+  // run().catch(console.dir);
+
+  try {
+    const database = client.db("golf-tracker");
+    const golfers = database.collection("golfers");
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { "First Name": "Brett" };
+    const golfer = await golfers.findOne(query);
+    console.log(golfer);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
 
-export default Schedule();
+export default function Schedule() {
+  // const { MongoClient, ServerApiVersion } = require("mongodb");
+  run().then(client.connect);
+  return <Label>Forgot your username or password?</Label>;
+}
