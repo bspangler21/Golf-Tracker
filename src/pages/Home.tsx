f6a8cd6a94ef35166942345d570c2ba024a88306
bspang <bspangler21@alumni.uwosh.edu>
2023-07-24 23:08:54 -0500

    Fix centering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,73 @@
 import {
-  DefaultButton,
-  IStackTokens,
-  Stack,
-  StackItem,
-  Text,
-  mergeStyleSets,
+	DefaultButton,
+	IStackTokens,
+	Stack,
+	StackItem,
+	Text,
+	mergeStyleSets,
 } from "@fluentui/react";
 import "./Home.css";
 // import { useNavigate } from "react-router-dom";
 
 // const nav = useNavigate();
 const classNames = mergeStyleSets({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    height: "100vh",
-    paddingLeft: "60%",
-    width: "100%",
-    color: "black",
-  },
-  buttonContainer: {
-    display: "flex",
-    flexGrow: "1",
-    paddingLeft: "36%",
-  },
-  button: {
-    width: "200px",
-    backgroundColor: "black",
-    color: "white",
-  },
+	container: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		flexDirection: "row",
+		height: "100vh",
+		// paddingLeft: "60%",
+		width: "100vw",
+		color: "black",
+	},
+	buttonContainer: {
+		display: "flex",
+		flexGrow: "1",
+		// paddingLeft: "36%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	button: {
+		width: "200px",
+		backgroundColor: "black",
+		color: "white",
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
 
 const stackStyles: IStackTokens = {
-  childrenGap: 20,
+	childrenGap: 20,
 };
 
 export default function Home() {
-  return (
-    <div className={classNames.container}>
-      <div className="column">
-        <h1 className="Header">Welcome to the Golf Tracker!</h1>
-        {/* <Stack horizontal styles={stackStyles} */}
-        <div className={classNames.buttonContainer}>
-          <Stack tokens={stackStyles}>
-            <Stack.Item>
-              <DefaultButton href={`/login`} className={classNames.button}>
-                Login
-              </DefaultButton>
-            </Stack.Item>
-            <Stack.Item>
-              <DefaultButton href={`/golfers`} className={classNames.button}>
-                View Golfers
-              </DefaultButton>
-            </Stack.Item>
-          </Stack>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classNames.container}>
+			<div className="column">
+				<h1 className="Header">Welcome to the Golf Tracker!</h1>
+				{/* <Stack horizontal styles={stackStyles} */}
+				<div className={classNames.buttonContainer}>
+					<Stack tokens={stackStyles}>
+						<Stack.Item>
+							<DefaultButton
+								href={`/login`}
+								className={classNames.button}
+							>
+								Login
+							</DefaultButton>
+						</Stack.Item>
+						<Stack.Item>
+							<DefaultButton
+								href={`/golfers`}
+								className={classNames.button}
+							>
+								View Golfers
+							</DefaultButton>
+						</Stack.Item>
+					</Stack>
+				</div>
+			</div>
+		</div>
+	);
 }
