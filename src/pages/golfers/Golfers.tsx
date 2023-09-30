e8d798a7a0d65c5e0788b5e47fed7c7686fa7eee
bspang <bspangler21@alumni.uwosh.edu>
2023-09-30 00:39:52 -0500

    GolfersAlt, Golfers, mock api

--- src/pages/golfers/Golfers.tsx
+++ src/pages/golfers/Golfers.tsx
@@ -1,59 +1,72 @@
 import {
-  Label,
-  MarqueeSelection,
-  TextField,
-  mergeStyleSets,
+	Label,
+	MarqueeSelection,
+	TextField,
+	mergeStyleSets,
 } from "@fluentui/react";
 import {
-  DetailsList,
-  DetailsListLayoutMode,
-  Selection,
-  IColumn,
-  SelectionMode,
+	DetailsList,
+	DetailsListLayoutMode,
+	Selection,
+	IColumn,
+	SelectionMode,
 } from "@fluentui/react/lib/DetailsList";
 import { IGolfer, mockGolfers } from "../../mockData/mockGolfers";
 import React from "react";
+import axios from "axios";
 
 const classNames = mergeStyleSets({
-  wrapper: {
-    height: "100vh",
-  },
-  headerWrapper: {
-    display: "flex",
-    flexDirection: "row",
-    // flexWrap: "nowrap",
-  },
-  headerSearchWrapper: {
-    width: "500px",
-    border: "none",
-    flexFlow: "0",
-  },
-  bodyWrapper: {
-    // background: "#FFFFFF",
-    // overflow: "scroll",
-    // width: "700px",
-    flexGrow: "1",
-    flexDirection: "row",
-  },
-  contentWrapper: {
-    padding: "32px 0px 100px 0px",
-    // paddingLeft: "50%",
-    paddingLeft: "Calc(Calc(0.18 * Calc(100vw - 700px)) + 7vw)",
-    paddingRight: "Calc(Calc(0.18 * Calc(100vw - 700px)) + 7vw)",
-    display: "flex",
-    flexDirection: "column",
-    flexWrap: "nowrap",
-    flexGrow: "1",
-    // verticalAlign: "middle"
-  },
+	wrapper: {
+		height: "100vh",
+	},
+	headerWrapper: {
+		display: "flex",
+		flexDirection: "row",
+		// flexWrap: "nowrap",
+	},
+	headerSearchWrapper: {
+		width: "500px",
+		border: "none",
+		flexFlow: "0",
+	},
+	bodyWrapper: {
+		// background: "#FFFFFF",
+		// overflow: "scroll",
+		// width: "700px",
+		flexGrow: "1",
+		flexDirection: "row",
+	},
+	contentWrapper: {
+		padding: "32px 0px 100px 0px",
+		// paddingLeft: "50%",
+		paddingLeft: "Calc(Calc(0.18 * Calc(100vw - 700px)) + 7vw)",
+		paddingRight: "Calc(Calc(0.18 * Calc(100vw - 700px)) + 7vw)",
+		display: "flex",
+		flexDirection: "column",
+		flexWrap: "nowrap",
+		flexGrow: "1",
+		// verticalAlign: "middle"
+	},
 });
 
+// fetch("https://jsonplaceholder.typicode.com/users")
+// 	.then((response) => response.json())
+// 	.then((json) => console.log(json));
+axios
+	.get("https://jsonplaceholder.typicode.com/users")
+	.then((response) => {
+		console.log("axios response", response.data);
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+
 export interface IGolferListState {
-  items: IGolfer[];
-  columns: IColumn[];
-  selectionDetails: string;
-  isModalSelection: boolean;
-  // isModalSelection: boolean;
+	items: IGolfer[];
+	columns: IColumn[];
+	selectionDetails: string;
+	isModalSelection: boolean;
+	// isModalSelection: boolean;
 }
 // import { MongoClient, ServerApiVersion } from "mongodb";
 
@@ -96,158 +109,163 @@ export interface IGolferListState {
 // }
 
 export default class Golfers extends React.Component<{}, IGolferListState> {
-  private _allItems: IGolfer[];
-  private _selection: Selection;
-  private _getKey(item: any, index?: number): string {
-    return item.key;
-  }
-  private _onChangeText = (
-    ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
-    text?: string
-  ): void => {
-    this.setState({
-      items: text
-        ? this._allItems.filter(
-            (i) =>
-              i.firstName.toLowerCase().indexOf(text) > -1 ||
-              i.lastName.toLowerCase().indexOf(text) > -1 ||
-              i.handicap.toString().indexOf(text) > -1
-          )
-        : this._allItems,
-    });
-  };
-  private _getSelectionDetails(): string {
-    return "View " + (this._selection.getSelection()[0]);
-  }
-  // private _columns: IColumn[];
-  // const { MongoClient, ServerApiVersion } = require("mongodb");
-  // run().then(client.connect);
-  constructor(props: {}) {
-    super(props);
+	private _allItems: IGolfer[];
+	private _selection: Selection;
+	private _getKey(item: any, index?: number): string {
+		return item.key;
+	}
+	private _onChangeText = (
+		ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+		text?: string
+	): void => {
+		this.setState({
+			items: text
+				? this._allItems.filter(
+						(i) =>
+							i.firstName.toLowerCase().indexOf(text) > -1 ||
+							i.lastName.toLowerCase().indexOf(text) > -1 ||
+							i.handicap.toString().indexOf(text) > -1
+				  )
+				: this._allItems,
+		});
+	};
+	private _getSelectionDetails(): string {
+		return "View " + this._selection.getSelection()[0];
+	}
+	// private _columns: IColumn[];
+	// const { MongoClient, ServerApiVersion } = require("mongodb");
+	// run().then(client.connect);
+	constructor(props: {}) {
+		super(props);
 
-    this._allItems = mockGolfers.sort((a, b) =>
-      a.handicap > b.handicap ? 1 : -1
-    );
+		this._allItems = mockGolfers.sort((a, b) =>
+			a.handicap > b.handicap ? 1 : -1
+		);
 
-    this._selection = new Selection({
-      onSelectionChanged: () => {
-        this.setState({
-          selectionDetails: this._getSelectionDetails(),
-          // selected: !selected,
-        });
-      },
-      getKey: this._getKey,
-    });
-    const columns: IColumn[] = [
-      {
-        key: "column1",
-        name: "First Name",
-        fieldName: "firstName",
-        minWidth: 100,
-        maxWidth: 200,
-        isSorted: true,
-        isSortedDescending: false,
-        isResizable: true,
-        onColumnClick: this._onColumnClick,
-      },
-      {
-        key: "column2",
-        name: "Last Name",
-        fieldName: "lastName",
-        minWidth: 100,
-        maxWidth: 200,
-        isSorted: true,
-        isSortedDescending: false,
-        isResizable: true,
-        onColumnClick: this._onColumnClick,
-      },
-      {
-        key: "column3",
-        name: "Handicap",
-        fieldName: "handicap",
-        minWidth: 100,
-        maxWidth: 200,
-        isSorted: true,
-        isSortedDescending: false,
-        isResizable: true,
-        onColumnClick: this._onColumnClick,
-      },
-    ];
+		this._selection = new Selection({
+			onSelectionChanged: () => {
+				this.setState({
+					selectionDetails: this._getSelectionDetails(),
+					// selected: !selected,
+				});
+			},
+			getKey: this._getKey,
+		});
+		const columns: IColumn[] = [
+			{
+				key: "column1",
+				name: "First Name",
+				fieldName: "firstName",
+				minWidth: 100,
+				maxWidth: 200,
+				isSorted: true,
+				isSortedDescending: false,
+				isResizable: true,
+				onColumnClick: this._onColumnClick,
+			},
+			{
+				key: "column2",
+				name: "Last Name",
+				fieldName: "lastName",
+				minWidth: 100,
+				maxWidth: 200,
+				isSorted: true,
+				isSortedDescending: false,
+				isResizable: true,
+				onColumnClick: this._onColumnClick,
+			},
+			{
+				key: "column3",
+				name: "Handicap",
+				fieldName: "handicap",
+				minWidth: 100,
+				maxWidth: 200,
+				isSorted: true,
+				isSortedDescending: false,
+				isResizable: true,
+				onColumnClick: this._onColumnClick,
+			},
+		];
 
-    this.state = {
-      items: this._allItems,
-      columns,
-      selectionDetails: this._getSelectionDetails(),
-      isModalSelection: false,
-    };
-  }
+		this.state = {
+			items: this._allItems,
+			columns,
+			selectionDetails: this._getSelectionDetails(),
+			isModalSelection: false,
+		};
+	}
 
-  private _onColumnClick = (
-    ev: React.MouseEvent<HTMLElement>,
-    column: IColumn
-  ): void => {
-    const { columns, items } = this.state;
-    const newColumns: IColumn[] = columns.slice();
-    const currColumn: IColumn = newColumns.filter(
-      (currCol) => column.key === currCol.key
-    )[0];
+	private _onColumnClick = (
+		ev: React.MouseEvent<HTMLElement>,
+		column: IColumn
+	): void => {
+		const { columns, items } = this.state;
+		const newColumns: IColumn[] = columns.slice();
+		const currColumn: IColumn = newColumns.filter(
+			(currCol) => column.key === currCol.key
+		)[0];
 
-    newColumns.forEach((newCol: IColumn) => {
-      if (newCol === currColumn) {
-        currColumn.isSortedDescending = !currColumn.isSortedDescending;
-        currColumn.isSorted = true;
-        const newItems = _copyAndSort(
-          items,
-          currColumn.fieldName!,
-          currColumn.isSortedDescending
-        );
-        this.setState({
-          columns: newColumns,
-          items: newItems,
-          selectionDetails: "",
-        });
-      }
-    });
+		newColumns.forEach((newCol: IColumn) => {
+			if (newCol === currColumn) {
+				currColumn.isSortedDescending = !currColumn.isSortedDescending;
+				currColumn.isSorted = true;
+				const newItems = _copyAndSort(
+					items,
+					currColumn.fieldName!,
+					currColumn.isSortedDescending
+				);
+				this.setState({
+					columns: newColumns,
+					items: newItems,
+					selectionDetails: "",
+				});
+			}
+		});
 
-    function _copyAndSort<IGolfer>(
-      items: IGolfer[],
-      columnKey: string,
-      isSortedDescending?: boolean
-    ): IGolfer[] {
-      const key = columnKey as keyof IGolfer;
-      return items
-        .slice(0)
-        .sort((a, b) =>
-          (isSortedDescending ? a[key] < b[key] : a[key] > b[key]) ? 1 : -1
-        );
-    }
-  };
+		function _copyAndSort<IGolfer>(
+			items: IGolfer[],
+			columnKey: string,
+			isSortedDescending?: boolean
+		): IGolfer[] {
+			const key = columnKey as keyof IGolfer;
+			return items
+				.slice(0)
+				.sort((a, b) =>
+					(isSortedDescending ? a[key] < b[key] : a[key] > b[key])
+						? 1
+						: -1
+				);
+		}
+	};
 
-  public render() {
-    const { items, columns, selectionDetails } = this.state;
+	public render() {
+		const { items, columns, selectionDetails } = this.state;
 
-    console.log("this._selection", this._selection.getSelection()[0]);
-    console.log("getKey", this._getKey);
+		console.log("this._selection", this._selection.getSelection()[0]);
+		console.log("getKey", this._getKey);
 
-    return (
-      <div className={classNames.wrapper}>
-        <div className={classNames.bodyWrapper}>
-          <div className={classNames.contentWrapper}>
-            <TextField label="Search:" onChange={this._onChangeText} />
-            <br></br>
-            <MarqueeSelection selection={this._selection}>
-              <DetailsList
-                items={items}
-                columns={columns}
-                selectionMode={SelectionMode.multiple}
-                // getKey={this._getKey}
-                setKey="multiple"
-                selection={this._selection}
-              />
-            </MarqueeSelection>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className={classNames.wrapper}>
+				<div className={classNames.bodyWrapper}>
+					<div className={classNames.contentWrapper}>
+						<TextField
+							label="Search:"
+							onChange={this._onChangeText}
+						/>
+						<br></br>
+						<MarqueeSelection selection={this._selection}>
+							<DetailsList
+								items={items}
+								columns={columns}
+								selectionMode={SelectionMode.multiple}
+								// getKey={this._getKey}
+								setKey="multiple"
+								selection={this._selection}
+							/>
+						</MarqueeSelection>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- src/pages/golfers/GolfersAlt.jsx
+++ src/pages/golfers/GolfersAlt.jsx
@@ -19,38 +19,38 @@ const golfersArray = mockGolfers;
 const GolfersAlt = () => {
 	const [golfers, setGolfers] = useState(golfersArray);
 
-	const addGolfer = () => {
-		setGolfers([
-			...golfers,
-			{
-				firstName: "Mick",
-				lastName: "Siech",
-				handicap: 11,
-			},
-		]);
-	};
+// 	const addGolfer = () => {
+// 		setGolfers([
+// 			...golfers,
+// 			{
+// 				firstName: "Mick",
+// 				lastName: "Siech",
+// 				handicap: 11,
+// 			},
+// 		]);
+// 	};
 
-	const 
-	return (
-		<>
-			<table>
-				<thead>
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Handicap</th>
-					</tr>
-				</thead>
-				<tbody>
-					{golfers.map((g) => (
-						<><tr key={g.firstName + g.lastName} /><td>{g.firstName}</td><td>{g.lastName}</td><td>{g.handicap}</td></>
-						</tr>
-					))}
-				</tbody>
-			</table>
+// 	const 
+// 	return (
+// 		<>
+// 			<table>
+// 				<thead>
+// 					<tr>
+// 						<th>First Name</th>
+// 						<th>Last Name</th>
+// 						<th>Handicap</th>
+// 					</tr>
+// 				</thead>
+// 				<tbody>
+// 					{golfers.map((g) => (
+// 						<><tr key={g.firstName + g.lastName} /><td>{g.firstName}</td><td>{g.lastName}</td><td>{g.handicap}</td></>
+// 						</tr>
+// 					))}
+// 				</tbody>
+// 			</table>
 
-		</>
-	);
+// 		</>
+// 	);
 };
 
 export default GolfersAlt;
