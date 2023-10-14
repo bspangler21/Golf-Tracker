667d6216e5558318fe2166b19fba48f7b73a9e22
BSPANGLER <bspangler21@alumni.uwosh.edu>
2023-10-14 01:21:21 -0500

    Updated Login href

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,9 @@
 import {
-	DefaultButton,
-	Label,
-	Stack,
-	TextField,
-	mergeStyleSets,
+  DefaultButton,
+  Label,
+  Stack,
+  TextField,
+  mergeStyleSets,
 } from "@fluentui/react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -11,91 +11,89 @@ import "./Login.css";
 // import * as golfLogo from "../../assets/golf.png";
 
 const classNames = mergeStyleSets({
-	container: {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		flexDirection: "row",
-		height: "100vh",
-		// paddingLeft: "400px",
-		// width: "100%",
-		width: "100vw",
-	},
-	fieldContainer: {
-		// paddingLeft: "25%",
-		// flexDirection: "row",
-		display: "flex",
-		flexGrow: "1",
-	},
-	textBox: {
-		width: "250px",
-		// display: "flex",
-		// flexGrow: "1",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	label: {
-		justifyContent: "center",
-		alignItems: "center",
-		// width: "250px",
-		display: "flex",
-		flexGrow: "1",
-	},
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    height: "100vh",
+    // paddingLeft: "400px",
+    // width: "100%",
+    width: "100vw",
+  },
+  fieldContainer: {
+    // paddingLeft: "25%",
+    // flexDirection: "row",
+    display: "flex",
+    flexGrow: "1",
+  },
+  textBox: {
+    width: "250px",
+    // display: "flex",
+    // flexGrow: "1",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  label: {
+    justifyContent: "center",
+    alignItems: "center",
+    // width: "250px",
+    display: "flex",
+    flexGrow: "1",
+  },
 });
 
 const loginStackTokens = {
-	childrenGap: 20,
+  childrenGap: 20,
 };
 
 const forgotLinkStyles = {
-	color: "blue",
+  color: "blue",
 };
 
 export default function Login() {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	console.log("username", username);
+  console.log("username", username);
 
-	return (
-		<div className={classNames.container}>
-			<div className="column">
-				<div className={classNames.fieldContainer}>
-					<Stack tokens={loginStackTokens}>
-						<TextField
-							placeholder="Username"
-							className={classNames.textBox}
-							required
-							onChange={(e, newValue) => {
-								setUsername(newValue || "");
-							}}
-							value={username}
-						></TextField>
-						<TextField
-							placeholder="Password"
-							required
-							className={classNames.textBox}
-							onChange={(e, newValue) => {
-								setPassword(newValue || "");
-							}}
-							value={password}
-						></TextField>
-						<DefaultButton
-							primary={true}
-							className={classNames.textBox}
-							onClick={() =>
-								console.log("submitted username ", username)
-							}
-							href={`/golfers`}
-						>
-							Login
-						</DefaultButton>
-						<Link to={`/`} className={classNames.label}>
-							<Label>Forgot your username or password?</Label>
-						</Link>
-					</Stack>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={classNames.container}>
+      <div className="column">
+        <div className={classNames.fieldContainer}>
+          <Stack tokens={loginStackTokens}>
+            <TextField
+              placeholder="Username"
+              className={classNames.textBox}
+              required
+              onChange={(e, newValue) => {
+                setUsername(newValue || "");
+              }}
+              value={username}
+            ></TextField>
+            <TextField
+              placeholder="Password"
+              required
+              className={classNames.textBox}
+              onChange={(e, newValue) => {
+                setPassword(newValue || "");
+              }}
+              value={password}
+            ></TextField>
+            <DefaultButton
+              primary={true}
+              className={classNames.textBox}
+              onClick={() => console.log("submitted username ", username)}
+              href={`/golfers-alt`}
+            >
+              Login
+            </DefaultButton>
+            <Link to={`/`} className={classNames.label}>
+              <Label>Forgot your username or password?</Label>
+            </Link>
+          </Stack>
+        </div>
+      </div>
+    </div>
+  );
 }
