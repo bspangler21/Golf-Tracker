48a70b9a47b198a0d2fe3c33a5a6c36fb877a3f3
bspang <bspangler21@alumni.uwosh.edu>
2023-12-15 17:28:59 -0600

    Add proxy to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			// Proxy all requests to index.html
+			"/": {
+				target: "/index.html",
+				changeOrigin: true,
+				rewrite: (path) => (path === "/" ? "/index.html" : path),
+			},
+		},
+	},
 });
