1d17797618e20dea2c047cb3cb3108ba7b81db9a
bspang <bspangler21@alumni.uwosh.edu>
2023-12-16 00:21:48 -0600

    Update Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Golfers from "./pages/golfers/Golfers";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Header from "./pageComponents/Header";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route path="/home" Component={Home}></Route>
 				<Route path="/golfers" Component={Golfers}></Route>
--- src/main.tsx
+++ src/main.tsx
@@ -1,70 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
-// import { RouterProvider, createBrowserRouter, BrowserRouter } from "react-router-dom";
-// import {
-// 	FluentProvider,
-// 	webLightTheme,
-// 	Button,
-// } from "@fluentui/react-components";
-// import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import Schedule from "./pages/golfers/Golfers";
-// import Golfers from "./pages/golfers/Golfers";
-// import Golfers from "./pages/golfers/Golfers";
-// import ScheduleList from "./pages/schedule/ScheduleList";
-// import GolferDetail from "./pages/golferDetail/GolferDetail";
-// import AddGolfer from "./pages/addGolfer/AddGolfer";
-// import Scorecard from "./pages/scorecard/Scorecard";
-// import Scorecard2 from "./pages/scorecard/Scorecard2";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-
-//     element: <Home />,
-//   },
-//   {
-//     path: "/app",
-//     element: <App />,
-//   },
-
-//   {
-//     path: "/home",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/login",
-//     element: <Login />,
-//   },
-//   {
-//     path: "/golfers",
-//     element: <Golfers />,
-//   },
-//   {
-//     path: "/schedule-list",
-//     element: <ScheduleList />,
-//   },
-//   {
-//     path: "/golfer-detail/:id",
-//     element: <GolferDetail />,
-//   },
-//   {
-//     path: "/add-golfer",
-//     element: <AddGolfer />,
-//   },
-//   {
-//     path: "/scorecard",
-//     element: <Scorecard />,
-//   },
-//   {
-//     path: "/scorecard2",
-//     element: <Scorecard2 courseName={undefined} players={undefined} />,
-//   },
-// ]);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
