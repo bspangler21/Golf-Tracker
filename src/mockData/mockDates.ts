3c958bd7747d2e04520a8726ad137f1128a9c582
bspang <bspangler21@alumni.uwosh.edu>
2023-12-27 22:41:39 -0600

    Update mock data to match MongoDB

--- src/mockData/mockDates.ts
+++ src/mockData/mockDates.ts
@@ -2,20 +2,20 @@ import { LeagueDate } from "../types/LeagueDate";
 
 export const mockDates: LeagueDate[] = [
 	{
-		id: "1",
-		leagueId: "1",
+		id: "658cfa555669234ca16a65cc",
+		leagueId: "658cf9da5669234ca16a65c8",
 		matchDate: new Date("09-05-2023"),
 		matchWeekNumber: 20,
 	},
 	{
-		id: "2",
-		leagueId: "1",
+		id: "658cfb135669234ca16a65ce",
+		leagueId: "658cf9da5669234ca16a65c8",
 		matchDate: new Date("09-12-2023"),
 		matchWeekNumber: 21,
 	},
 	{
-		id: "3",
-		leagueId: "1",
+		id: "658cfb345669234ca16a65cf",
+		leagueId: "658cf9da5669234ca16a65c8",
 		matchDate: new Date("09-19-2023"),
 		matchWeekNumber: 22,
 	},
--- src/mockData/mockMatches.ts
+++ src/mockData/mockMatches.ts
@@ -2,23 +2,23 @@ import { Match } from "../types/Match";
 
 export const mockMatches: Match[] = [
 	{
-		id: "1",
-		leagueId: "1",
-		dateId: "1",
+		id: "658cfbd35669234ca16a65d4",
+		leagueId: "658cf9da5669234ca16a65c8",
+		dateId: "658cfa555669234ca16a65cc",
 		golfer1Id: "6434dce4c909c69981fe99ce",
 		golfer2Id: "6434dce4c909c69981fe99cf",
 	},
 	{
-		id: "2",
-		leagueId: "1",
-		dateId: "1",
+		id: "658cfc385669234ca16a65d5",
+		leagueId: "658cf9da5669234ca16a65c8",
+		dateId: "658cfa555669234ca16a65cc",
 		golfer1Id: "6434dce4c909c69981fe99d0",
 		golfer2Id: "65891fbc1625bd334810e52a",
 	},
 	{
 		id: "3",
-		leagueId: "1",
-		dateId: "2",
+		leagueId: "658cfc635669234ca16a65d6",
+		dateId: "658cfb135669234ca16a65ce",
 		golfer1Id: "6434dce4c909c69981fe99ce",
 		golfer2Id: "6434dce4c909c69981fe99d0",
 	},
