139aa89171087b4947990813f8468b15016b63da
bspang <bspangler21@alumni.uwosh.edu>
2024-01-02 00:32:07 -0600

    Refactor shuffleArray function and update comments

--- src/pages/schedule/ScheduleList.tsx
+++ src/pages/schedule/ScheduleList.tsx
@@ -108,14 +108,14 @@ const ScheduleList = () => {
 	function shuffleArray(array) {
 		for (let i = array.length - 1; i > 0; i--) {
 			/**
-			 * Generates a random number between 0 and the specified maximum value.
-			 *
+			 * Generates a random number between 0 and the specified maximum value (inclusive).
 			 * @param {number} max - The maximum value for the random number.
 			 * @returns {number} - The generated random number.
 			 */
 			const j = Math.floor(Math.random() * (i + 1));
-			/* Swaps the elements at the given indices in the array.
-			 *
+			/**
+			 * Swaps the elements at the given indices in the array.
+			 * 
 			 * @param array - The array in which the elements should be swapped.
 			 * @param i - The index of the first element to be swapped.
 			 * @param j - The index of the second element to be swapped.
