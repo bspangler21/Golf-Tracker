d68aaf657bad6aaeb4164f07f2c029c3698a3ae8
BSPANGLER <bspangler21@alumni.uwosh.edu>
2024-03-24 23:30:53 -0500

    Refactor formatDate function to use TypeScript and add DateObject interface

--- src/util/generalUtils.ts
+++ src/util/generalUtils.ts
@@ -1,10 +1,16 @@
-export const formatDate = (dateString) => {
-	const date = new Date(dateString);
-	const month = date.getMonth() + 1; // Months are 0-based in JavaScript
-	const day = date.getDate();
-	const year = date.getFullYear();
+export interface DateObject {
+  month: number;
+  day: number;
+  year: number;
+}
 
-	return `${month}/${day}/${year}`;
+export const formatDate = (dateString: string): string => {
+  const date: Date = new Date(dateString);
+  const month: number = date.getMonth() + 1; // Months are 0-based in JavaScript
+  const day: number = date.getDate();
+  const year: number = date.getFullYear();
+
+  return `${month}/${day}/${year}`;
 };
 
 export const teeTimes = ["3:30 PM", "3:37 PM", "3:45 PM"];
